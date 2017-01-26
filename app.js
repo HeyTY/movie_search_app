3f3e313a775d810e1d788589748fbef1a5f32a4f
Ty Le <anhtuan.n.le@gmail.com>
2017-01-25 20:36:58 -0500

    Added comments function

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.get("/chart/:id", function(req, res){
 app.get("/chart/:id/comments/new", function(req, res){
 	// Find movie by ID
 
-	Movie.findById(req.params.id, function(err, movies){
+	Movie.findById(req.params.id, function(err, movie){
 		if (err) {
 			console.log(err);
 			res.redirect("/chart");
@@ -132,9 +132,26 @@ app.get("/chart/:id/comments/new", function(req, res){
 
 // COMMENT POST ROUTE
 
-
-
-
+app.post("/chart/:id/comments", function(req, res){
+	// lookup movie by ID
+	Movie.findById(req.params.id, function (err, movie){
+		if (err){
+			console.log(err);
+			res.redirect("/chart");
+		} else {
+	// create a new comment
+	Comment.create(req.body.comment, function(err, comment){
+		if (err) {
+			console.log(err);
+		} else {
+			movie.comments.push(comment);
+			movie.save();
+			res.redirect("/chart/" + movie._id);	
+		}		
+		});
+		}
+	;})
+})
 
 
 
